b205763b13cd90c1836fbd92cfa4a4b7dc2e7f49
Bar Kobi <barkobi102@gmail.com>
2023-09-06 20:38:26 +0300

    push

--- lib/widgets/business_item.dart
+++ lib/widgets/business_item.dart
@@ -25,8 +25,6 @@ class BusinesListItem extends StatelessWidget {
       child: ElevatedButton(
         onPressed: onSelect,
         style: ElevatedButton.styleFrom(
-          backgroundColor: Theme.of(context).primaryColor,
-          foregroundColor: Theme.of(context).colorScheme.onPrimary,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
           ),
